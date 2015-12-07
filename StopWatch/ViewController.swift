0235d48dbcbf5e6908df367b80798a4e7c62c54d
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 22:10:19 +0200

    StopWatchAppiOS
    simple stop watch timer

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,6 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    
+    var time = 0
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        time = 0
+        
+        timerLabel.text = "0"
+    }
+    
+    @IBAction func pauseButton(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    
+    
+    
+    func increaseTimer() {
+        
+        time++
+        
+        timerLabel.text = "\(time)"
+        
+    }
+    
+    @IBAction func startButton(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
